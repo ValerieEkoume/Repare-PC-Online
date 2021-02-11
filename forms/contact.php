9cc2740800fb86f551ea7e466c161299f33858e6
ValerieEkoume <72264813+ValerieEkoume@users.noreply.github.com>
2021-02-11 16:30:10 +0100

    commit -m 'vendor'

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,35 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+// Vérification de tous les champs s'assurer qu'ils soient bien remplis
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+if (empty($_POST['email']) ||
+    empty($_POST['name']) ||
+    //empty($_POST['phone'])   ||
+    empty($_POST['message']) ||
+    !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) //Si tous les champs ne sont pas remplis retourne faux
+{
+  return false;
+}
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+$name=strip_tags(htmlspecialchars($_POST['name']));
+$email_address=strip_tags(htmlspecialchars($_POST['email']));
+//$phone = strip_tags(htmlspecialchars($_POST['phone']));
+$message=strip_tags(htmlspecialchars($_POST['message']));
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+// Pour créer l'email et pour voir l'envoyer
+$to='valerie.ekoume@gmail.com'; // Ajout de l'adresse mail qui va recevoir les messages
+$email_subject="Website Contact Form:  $name";
+$email_body="Vous avez reçu un nouveau message depuis le formulaire de contact de votre site Web.\n\n" .
+    "Voici les détails:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
+$headers="From: valerie.ekoume@gmail.com\n"; // This is the email address the generated message will be from.
+// We recommend using something like noreply@yourdomain.com.
+$headers.="Reply-To: $email_address";
+$send=mail($to, $email_subject, $email_body, $headers);
 
-  echo $contact->send();
-?>
+//Conditions pour checker que le mail a bien été envoyé
+if ($send === true) {
+  header('Location: form.php?success=true');
+} else {
+  header('Location: form.php?success=false');
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,78 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'r00t/file-explorer',
+  ),
+  'versions' => 
+  array (
+    'altorouter/altorouter' => 
+    array (
+      'pretty_version' => '2.0.1',
+      'version' => '2.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '127f6e96998708a31ef32252985bea82e3b03888',
+    ),
+    'filp/whoops' => 
+    array (
+      'pretty_version' => '2.9.2',
+      'version' => '2.9.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'df7933820090489623ce0be5e85c7e693638e536',
+    ),
+    'psr/log' => 
+    array (
+      'pretty_version' => '1.1.3',
+      'version' => '1.1.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0f73288fd15629204f9d42b7055f72dacbe811fc',
+    ),
+    'r00t/file-explorer' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'symfony/polyfill-mbstring' => 
+    array (
+      'pretty_version' => 'v1.22.0',
+      'version' => '1.22.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f377a3dd1fde44d37b9831d68dc8dea3ffd28e13',
+    ),
+    'symfony/polyfill-php80' => 
+    array (
+      'pretty_version' => 'v1.22.0',
+      'version' => '1.22.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'dc3063ba22c2a1fd2f45ed856374d79114998f91',
+    ),
+    'symfony/var-dumper' => 
+    array (
+      'pretty_version' => 'v5.2.3',
+      'version' => '5.2.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '72ca213014a92223a5d18651ce79ef441c12b694',
+    ),
+  ),
+);
